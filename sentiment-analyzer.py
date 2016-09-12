ba62e3379a94dae60cec02d8cbd58e955a11ab04
Alexandre Tea <alexandre.qtea@gmail.com>
2016-09-12 21:12:08 +0200

    add doc

--- sentiment-analyzer.py
+++ sentiment-analyzer.py
@@ -4,7 +4,7 @@
 # File:     /Users/alexandretea/Work/sentiment-analysis/sentiment-analyzer.py
 # Purpose:  TODO (a one-line explanation)
 # Created:  2016-09-11 18:07:59
-# Modified: 2016-09-12 00:08:29
+# Modified: 2016-09-12 21:11:58
 
 import sys
 import argparse
@@ -12,7 +12,12 @@ import nltk
 import nlp.sentiment as sentiment
 
 __doc__ = """
-    Zobb
+    This script can be used to classify a sentence as positive or negative.
+    It has to be trained with datasets from file with the following format:
+        0|1 sentence newline
+    Example:
+        1 I love Python
+        0 Gary is my enemy
 """
 
 
